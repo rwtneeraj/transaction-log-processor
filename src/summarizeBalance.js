26784c4f9ec685f3d088cd2a1fbb4f7147506e79
rwtneeraj <183721769+rwtneeraj@users.noreply.github.com>
2025-01-18 16:39:33 +0530

    handle error by adding try catch

--- src/summarizeBalance.js
+++ src/summarizeBalance.js
@@ -1,5 +1,18 @@
-
-export const getBalanceOfCurrId = function (transaction) {
-  return "debit" in transaction ? 0 : transaction.amount;
+const isValidType = function (type) {
+  return type === "debit" || type === "credit";
 };
 
+export const getBalanceOfCurrId = function (transaction) {
+  try {
+    if (isValidType(transaction.type)) {
+      return transaction.type === "debit"
+        ? -transaction.amount
+        : transaction.amount;
+    }
+    throw `invalid type`
+  } catch(error) {
+    return `Transaction ID ${transaction.id} has invalid data: ${error}`;
+  }
+};
+
+// const update
