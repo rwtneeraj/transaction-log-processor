1d34e591583799251dea5adf45dd27787b0224c7
rwtneeraj <183721769+rwtneeraj@users.noreply.github.com>
2025-01-18 17:41:46 +0530

    added testcases for updateBalance

--- test/summarizeBalance_test.js
+++ test/summarizeBalance_test.js
@@ -1,21 +1,37 @@
-import { getBalanceOfCurrId } from "../src/summarizeBalance.js";
+import { getBalanceOfCurrId, updateBalances } from "../src/summarizeBalance.js";
 import { describe, test } from "jsr:@std/testing/bdd";
 import { assertEquals } from "jsr:@std/assert";
 
 describe("testing getBalanceOfCurrId", () => {
   test("should balance with negative sign, if account type is debit", () => {
     const sampleData = { type: "debit", amount: 200 };
-    assertEquals(getBalanceOfCurrId(sampleData),-200)
-  })
+    assertEquals(getBalanceOfCurrId(sampleData), -200);
+  });
 
-  test("should balance with positive sign, if account type is credit",() =>{
+  test("should balance with positive sign, if account type is credit", () => {
     const sampleData = { type: "credit", amount: 500 };
-    assertEquals(getBalanceOfCurrId(sampleData),500)
+    assertEquals(getBalanceOfCurrId(sampleData), 500);
   });
 
-  test("should throw error , if account type is invalid",() =>{
-    const sampleData = { id : "TXN001", type: "Null", amount: 500 };
-    const expectedOutput = `Transaction ID TXN001 has invalid data: invalid type`
-    assertEquals(getBalanceOfCurrId(sampleData),expectedOutput)
+  test("should throw error , if account type is invalid", () => {
+    const sampleData = { id: "TXN001", type: "Null", amount: 500 };
+    assertEquals(getBalanceOfCurrId(sampleData), 0);
   });
-})
+});
+
+describe("testing updateBalances()", () => {
+  test("should decrease the balance of user, if transaction type is debit", () => {
+    const sampleData = { id: "TXN001", type: "debit", amount: 1200.0 };
+    assertEquals(updateBalances({ TXN001: 1500 }, sampleData), { TXN001: 300 });
+  });
+
+  test("should decrease the balance of user, if transaction type is debit", () => {
+    const sampleData = { id: "TXN002", type: "credit", amount: 1200.0 };
+    assertEquals(updateBalances({ TXN002: 0 }, sampleData), { TXN002: 1200 });
+  });
+
+  test("should catch error , if data is invalid", () => {
+    const expectedOutput = `error : transactions has invalid data`;
+    assertEquals(updateBalances({},NaN),expectedOutput);
+  });
+});
